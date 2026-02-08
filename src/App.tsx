d6bb3bc0411728f12c6ebff2e5c75f2dc8d89da2
0x-70da <mahmoudAnonymous0@gmail.com>
2026-02-08 09:05:54 +0200

    fixed bug in light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <div className="dark">
+        <div>
           <Toaster />
           <Sonner />
           <BrowserRouter>
